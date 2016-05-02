92a386aa500bde7487d5440db29c63d58c371fa6
Chris Harrell <chris.harrell@sq1.com>
2016-05-02 19:23:00 +0000

    Updated package version and transpiled sources

--- dist/lib/reports-filter.js
+++ dist/lib/reports-filter.js
@@ -9,6 +9,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var co = require('co');
 var filterByMag = require('./filters/magnitude');
 var filterByTime = require('./filters/time');
+var filterByLat = require('./filters/latitude');
+var filterByLng = require('./filters/longitude');
 
 module.exports = function reportsRequest(data, filters) {
 	return co(_regenerator2.default.mark(function usgsRequest() {
@@ -32,9 +34,18 @@ module.exports = function reportsRequest(data, filters) {
 						if (filters.minTime || filters.maxTime) {
 							data = filterByTime(data, filters.minTime, filters.maxTime);
 						}
+
+						if (filters.minLat || filters.maxLat) {
+							data = filterByLat(data, filters.minLat, filters.maxLat);
+						}
+
+						if (filters.minLng || filters.maxLng) {
+							data = filterByLng(data, filters.minLng, filters.maxLng);
+						}
+
 						return _context.abrupt('return', data);
 
-					case 5:
+					case 7:
 					case 'end':
 						return _context.stop();
 				}
